ec52f2be5f1f42a0536d057db52042c4c1aefaf1
nk_q <14609624+nk_q@user.noreply.gitee.com>
2025-04-29 18:29:11 +0800

    缺页中断互斥处理

--- src/main/java/org/example/oscdspring/memory_management/MMU.java
+++ src/main/java/org/example/oscdspring/memory_management/MMU.java
@@ -117,6 +117,7 @@ class PTR {
     public int getLastPageSize() {
         return lastPageSize;
     }
+
     public void setLastPageSize(int lastPageSize) {
         this.lastPageSize = lastPageSize;
     }
@@ -260,6 +261,10 @@ class TLBEntry {
 
 
 class PageFaultHandler {
+
+    // 互斥处理
+    private static final Object lock = new Object();
+
     // 处理缺页中断，参数：页表地址，tlb，页号
 
     // 1. 如果物理内存已满，且该进程没有被调入物理内存的页面，无法局部替换，返回-1;
@@ -270,75 +275,75 @@ class PageFaultHandler {
     // 将磁盘块读入内存
     // 更新页表项，更新TLB
     static public boolean handlePageFault(int pageTableAddress, TLB tlb, int pageNumber) {
-        // 读取页表
-        PageTable pageTable = PageTableArea.getInstance().getPageTable(pageTableAddress);
-        // 读取出错的页表项
-        PageTableEntry faultPageTableEntry = pageTable.getEntry(pageNumber, false);
+        synchronized (lock) {
+            // 读取页表
+            PageTable pageTable = PageTableArea.getInstance().getPageTable(pageTableAddress);
+            // 读取出错的页表项
+            PageTableEntry faultPageTableEntry = pageTable.getEntry(pageNumber, false);
 
-        boolean PageHasEmptyFrame = pageTable.hasEmptyFrame();
-        boolean MemoryHasEmptyFrame = Memory.getInstance().findEmptyBlock() != -1;
-        boolean HasValidPage = pageTable.hasValidPage();
+            boolean PageHasEmptyFrame = pageTable.hasEmptyFrame();
+            boolean MemoryHasEmptyFrame = Memory.getInstance().findEmptyBlock() != -1;
+            boolean HasValidPage = pageTable.hasValidPage();
 
-        if ((!HasValidPage) && (!MemoryHasEmptyFrame)) {
-            LogEmitterService.getInstance().sendLog("物理内存已满，且未给本进程分配物理内存，无法替换页面");
-            return false;
-        }
-
-        // 获取调入页面的页号
-        int freeFrame;
-        // 物理内存已满或者页表内存用完，需要调出页面
-        if ((!MemoryHasEmptyFrame) || (!PageHasEmptyFrame)) {
-            int replacePage = pageTable.getReplacePage();
-            PageTableEntry replacePageTableEntry = pageTable.getEntry(replacePage, false);
-            if (replacePageTableEntry == null) {
-                System.err.println("按理说这里不可能false， 如果出现了说明有BUG，反馈给我，我会修改");
+            if ((!HasValidPage) && (!MemoryHasEmptyFrame)) {
+                LogEmitterService.getInstance().sendLog("物理内存已满，且未给本进程分配物理内存，无法替换页面");
                 return false;
             }
-            freeFrame = replacePageTableEntry.getFrameNumber();
-            // 只有被修改的页面才需要写回磁盘，否则直接替换
-            if (replacePageTableEntry.isDirty()) {
-                // 如果被修改的页面此前没有对应，需要分配磁盘块
-                if (!replacePageTableEntry.isAllocatedDisk()) {
-                    int diskAddress = getFileSystem().allocateBlock();
-                    replacePageTableEntry.setDiskAddress(diskAddress);
-                    replacePageTableEntry.AllocatedDisk();
 
+            // 获取调入页面的页号
+            int freeFrame;
+            // 物理内存已满或者页表内存用完，需要调出页面
+            if ((!MemoryHasEmptyFrame) || (!PageHasEmptyFrame)) {
+                int replacePage = pageTable.getReplacePage();
+                PageTableEntry replacePageTableEntry = pageTable.getEntry(replacePage, false);
+                if (replacePageTableEntry == null) {
+                    System.err.println("按理说这里不可能false， 如果出现了说明有BUG，反馈给我，我会修改");
+                    return false;
                 }
-                getFileSystem().writeBlock(replacePageTableEntry.getDiskAddress(), Memory.getInstance().readBlock(replacePageTableEntry.getFrameNumber()));
+                freeFrame = replacePageTableEntry.getFrameNumber();
+                // 只有被修改的页面才需要写回磁盘，否则直接替换
+                if (replacePageTableEntry.isDirty()) {
+                    // 如果被修改的页面此前没有对应，需要分配磁盘块
+                    if (!replacePageTableEntry.isAllocatedDisk()) {
+                        int diskAddress = getFileSystem().allocateBlock();
+                        replacePageTableEntry.setDiskAddress(diskAddress);
+                        replacePageTableEntry.AllocatedDisk();
+
+                    }
+                    getFileSystem().writeBlock(replacePageTableEntry.getDiskAddress(), Memory.getInstance().readBlock(replacePageTableEntry.getFrameNumber()));
+                }
+                // 更新内存
+                Memory.getInstance().freeBlock(freeFrame);
+                // 更新页表项
+                replacePageTableEntry.setValid(false);
+                // 更新TLB
+                tlb.deleteEntry(replacePage);
             }
-            // 更新内存
-            Memory.getInstance().freeBlock(freeFrame);
-            // 更新页表项
-            replacePageTableEntry.setValid(false);
+            // 物理内存有空闲块，直接分配
+            else {
+                freeFrame = Memory.getInstance().findEmptyBlock();
+                pageTable.addMemoryUsed();
+            }
+
+            // 将磁盘块读入内存
+            if (faultPageTableEntry.getDiskAddress() != -1) {
+                Memory.getInstance().writeBlock(freeFrame, getFileSystem().readBlock(faultPageTableEntry.getDiskAddress()), pageTable.getPid(), pageNumber);
+            }
+            else{// 更新内存块状态
+                Memory.getInstance().updateBlock(freeFrame, pageTable.getPid(), pageNumber);
+            }
+
+            // 更新页表项，
+            faultPageTableEntry.setFrameNumber(freeFrame);
+            faultPageTableEntry.setValid(true);
+            faultPageTableEntry.setAccessed(true);
+            faultPageTableEntry.setDirty(false);
+
             // 更新TLB
-            tlb.deleteEntry(replacePage);
+            tlb.addEntry(pageNumber, freeFrame);
+            return true;
         }
-        // 物理内存有空闲块，直接分配
-        else {
-            freeFrame = Memory.getInstance().findEmptyBlock();
-            pageTable.addMemoryUsed();
-        }
-
-        // 将磁盘块读入内存
-        if (faultPageTableEntry.getDiskAddress() != -1) {
-            Memory.getInstance().writeBlock(freeFrame, getFileSystem().readBlock(faultPageTableEntry.getDiskAddress()), pageTable.getPid(), pageNumber);
-        }
-        else{// 更新内存块状态
-            Memory.getInstance().updateBlock(freeFrame, pageTable.getPid(), pageNumber);
-        }
-
-        // 更新页表项，
-        faultPageTableEntry.setFrameNumber(freeFrame);
-        faultPageTableEntry.setValid(true);
-        faultPageTableEntry.setAccessed(true);
-        faultPageTableEntry.setDirty(false);
-
-        // 更新TLB
-        tlb.addEntry(pageNumber, freeFrame);
-        return true;
-
     }
-
 }
 
 
